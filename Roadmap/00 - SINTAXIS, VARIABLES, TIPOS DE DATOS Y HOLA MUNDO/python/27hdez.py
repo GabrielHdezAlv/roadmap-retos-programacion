210e8d93c28f3575e5c45d1ffe6490cefdf59bf4
gHernandezPC <ghernandez@plugcore.com>
2024-11-03 19:54:06 +0000

    #00 - Python - 27hdez.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/27hdez.py
@@ -0,0 +1,41 @@
+# https://www.python.org
+
+# Comentario en una línea
+
+"""
+Comentario
+de
+multilíneas
+"""
+
+'''
+Comentario
+multilíneas
+con caracteres especiales: ""
+'''
+
+CONSTANTE = "Constante"
+
+# Numérico
+int = 1
+float = 1.2
+complex = 1 + 2j #Números complejos donde "j" es una unidad imaginaria
+
+#Booleanos
+bool = True
+
+#Secuencias
+str = "Hello World"
+list = [1,2,3]
+tuple = (1, "b", 2) #Tuplas ordenadas e inmutables de elementos
+range(4) #Secuencia de números en un cirto rango
+
+#Conjuntos
+set = {1,2,3} #Elementos únicos y no ordenados
+frozenset({1,2,3}) #Igual que un 'set' pero no se puede modificar
+
+#Diccionarios
+dict = {"nombre": "Alice", "edad": 25} #Estructura de datos
+
+# Escritura por pantalla
+print("Hello World")
